865f4e7c9efb6e0a55ffaa6faa000f27ba852bb7
harisyammnv <harisyam.bphc@gmail.com>
2022-07-03 17:22:15 +0200

    test git api

--- publisher.py
+++ publisher.py
@@ -16,7 +16,10 @@ g = Github(options["GITHUB_TOKEN"])
 repo_name = options["GITHUB_REPOSITORY"]
 repo = g.get_repo(repo_name)
 pulls = repo.get_pulls(state="open", sort='created')
-pr = repo.get_pull(pulls[-1].number)
+for pull in pulls:
+    print(pull)
+    nr = pull.number
+pr = repo.get_pull(nr)
 summary, result = parse_reports(options)
 
 pr.create_issue_comment(result)
